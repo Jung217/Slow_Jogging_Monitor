11d45a27061ad096d8cf9d543800ae1f33603968
Jung217 <alex24922665@gmail.com>
2024-03-25 01:04:59 +0800

    Update schedule_bar

--- property/schedule_bar/schedule_bar.ino
+++ property/schedule_bar/schedule_bar.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h> // Hardware-specific library
 #include <SPI.h>
+
  
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
  
@@ -15,30 +16,67 @@ static const unsigned char PROGMEM O2_bmp[] = {
   0x0f, 0xe0, 0x00, 0x00, 0x07, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-void fillSegment(int x,int y,unsigned int colour)
+void fillSegment(int x,int y,unsigned int colour) //  tft.fillScreen(TFT_BLACK);
 {
   int y2 = 10;
   for (int i=0 ; i<+300 ; i++)
   {
- 
     tft.fillRect(x, y,  i,  y2, colour);
-    //如果在这里加个delay会出现扇形动画
     delay(20);
   }
 }
- 
+
+void introView()
+{
+  tft.fillScreen(TFT_BLACK);
+  delay(100);
+  for(int i=170 ; i>=0 ; i--)
+  {
+    tft.drawPixel(30, i, TFT_ORANGE);
+    tft.drawPixel(31, i, TFT_ORANGE);
+    tft.drawPixel(32, i, TFT_ORANGE);
+    delay(2.5);
+  }
+  for(int i=0 ; i<=320 ; i++)
+  {
+    tft.drawPixel(i, 149, TFT_ORANGE);
+    tft.drawPixel(i, 150, TFT_ORANGE);
+    tft.drawPixel(i, 151, TFT_ORANGE);
+    delay(2.5);
+  }
+
+  delay(150);
+  
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(5);
+  tft.setCursor(50, 10);
+  tft.print("Slow");
+  delay(200);
+  tft.setCursor(50, 56);
+  tft.print("Jogging");
+  delay(100);
+  tft.setCursor(50, 102);
+  tft.print("Monitor");
+  delay(150);
+  tft.setTextSize(1);
+  tft.setCursor(50, 160);
+  tft.print("Ver.1.0.1"); // version
+
+  delay(1500);
+
+  tft.fillScreen(TFT_BLACK);
+}
 void setup(void)
 {
   tft.init();
   tft.setRotation(3);
   tft.fillScreen(TFT_BLACK);
-  tft.drawBitmap(0, 0, mini, 100, 100, TFT_WHITE);
- 
+
+  delay(500);
+  introView();
 }
  
 void loop()
 {
-  //tft.fillScreen(TFT_BLACK);
-  fillSegment(10, 85, TFT_WHITE);
 }
  
